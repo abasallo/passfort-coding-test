bba2f69da12164f17f01ec019c60a650f9af3645
Alvaro Basallo <alvaro@basallo.es>
2023-04-03 10:12:52 +0100

    Minimal ExpressJS server with heartbeat.

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv'
+
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+import index from './express/routes'
+
+import { errorMiddleware } from './express/errors'
+
+dotenv.config()
+
+export const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/', index)
+
+app.use(errorMiddleware)
+
+export default app
